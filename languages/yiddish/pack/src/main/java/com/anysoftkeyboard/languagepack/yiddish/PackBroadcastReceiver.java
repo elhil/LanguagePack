8d3e5abe9a1e53326384388852680d27d3c6b011
Eliot Hills <j.eliot.hills@gmail.com>
2019-12-27 15:21:08 -0500

    [WIP] add yiddish

---
+++ languages/yiddish/pack/src/main/java/com/anysoftkeyboard/languagepack/yiddish/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.yiddish;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
